324eb8df0a53054f2123d3a6f4ecea7b2f023212
Roman Yacenko <ryacenko3@gmail.com>
2022-10-06 14:31:01 +0300

    sended calculated object

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -6,10 +6,20 @@ const router = Router();
 router.get(
   "/path", async (req, res) => {
       try {
+        let obj = {};
         const response = await axios.get(req.query.str)
         const html = await response.data;
-        const splittedHtml = html.toLowerCase().split('');
-        res.status(200).json({ html: splittedHtml });
+        html.toLowerCase().split('').forEach((item) => {
+          if (item in obj) {
+            obj = { ...obj, [item]: obj[item] + 1 };
+          } else {
+            obj = { ...obj, [item]: 1 };
+          }
+        });
+
+        Object.keys(obj).map(item => obj[item] /= html.length)
+
+        res.status(200).json({ html: obj });
       } catch {
         res.status(404).send('Error');
       }
